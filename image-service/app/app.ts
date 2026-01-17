f79a885d814b47348653c3e4b5776d7b2e9b8ce8
Debanjan Das <microsoft2420@gmail.com>
2026-01-18 00:13:03 +0530

    feat:Added global middleware function to authenticate the user

--- image-service/app/app.ts
+++ image-service/app/app.ts
@@ -1,12 +1,23 @@
 import express from 'express'
 import cookieParser from 'cookie-parser';
 import { errorHandler } from 'devdad-express-utils'
+import { validateUser } from './src/middleware/validateUser.middleware.js'
+import multer from 'multer'
+
+const storage = multer.memoryStorage()
+const upload = multer({storage: storage}) //using buffer for the data
 
 const app = express() 
+
 
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser());
 app.use(errorHandler);
+app.use(validateUser);
+
+app.get('/' , (req , res) => {
+    return res.send("ok")
+})
 
 export {
     app
---
+++ image-service/app/src/middleware/validateUser.middleware.ts
@@ -0,0 +1,38 @@
+import { sendError } from 'devdad-express-utils'
+import type { Request, Response, NextFunction } from 'express'
+import axios from 'axios'
+
+const validateUser = async (req: Request, res: Response, next: NextFunction) => {
+    // console.log(req);    
+    const token = req.cookies.accessToken;
+    if (!token) {
+        return sendError(res, "AccessToken not found", 501, null)
+    }
+    console.log(req.hostname, req.httpVersion, req.protocol, req.path);
+    const url = `${req.protocol}://${req.hostname}:5600/api/v1/user/validate`
+    console.log(url);
+    axios({
+        method: 'post',
+        url: url,
+        headers: {
+            Cookie: `accessToken=${token}`
+        },
+        withCredentials: true
+    }).then((data) => {
+        if(data.data.success === true){
+            next()
+        }else{
+            return sendError(res, "User has loged out already", 401, null)
+        }
+    })
+        .catch((err) => {
+            if (err.status === 401) {
+                return sendError(res, "User has loged out already", 401, null)
+            } else {
+                return sendError(res, "User validation failed", 500, null)
+            }
+        })
+}
+export {
+    validateUser
+}
