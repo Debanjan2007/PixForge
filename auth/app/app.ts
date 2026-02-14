5f331522e0178e243ba49a942c1db8f3cf0c07f7
Debanjan Das <microsoft2420@gmail.com>
2026-02-15 03:10:51 +0530

    Update app.ts

--- auth/app/app.ts
+++ auth/app/app.ts
@@ -1,4 +1,4 @@
-import express from 'express' // importin
+import express from 'express' 
 import { errorHandler } from "devdad-express-utils";
 import { router as user} from './src/routes/user.routes.js'
 import { router as healthcheck} from './src/routes/healthcheck.routes.js'
