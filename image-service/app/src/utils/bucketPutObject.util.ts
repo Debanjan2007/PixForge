dc2d87f5be7e2f6b563fcba54e88e7edfa7e8afe
Debanjan Das <microsoft2420@gmail.com>
2026-04-18 00:49:05 +0530

    Change the putobjectcommand config by removing the headers

--- image-service/app/src/utils/bucketPutObject.util.ts
+++ image-service/app/src/utils/bucketPutObject.util.ts
@@ -1,18 +1,16 @@
 import {PutObjectCommand} from "@aws-sdk/client-s3";
 import {getSignedUrl} from "@aws-sdk/s3-request-presigner";
-import {s3client} from "../../index.js";
+import {presignClient} from "../../index.js";
 import {v4 as uuidV4} from 'uuid'
 // Put or upload an object to a bucket.
-const uploadObjectinBucket = async (bucketName: string, userid: string , filetype: string) => {
+const uploadObjectinBucket = async (bucketName: string, userid: string) => {
     try {
         const uniqueKey: string = `${userid}_${uuidV4()}`
         const command = new PutObjectCommand({
             Bucket: bucketName,
             Key: uniqueKey,
-            ContentDisposition: "inline",
-            ContentType: filetype,
         })
-        const presignedUrl = await getSignedUrl(s3client, command, {expiresIn: 600}) // 10 minutes
+        const presignedUrl = await getSignedUrl(presignClient, command, {expiresIn: 600}) // 10 minutes
         return {presignedUrl, uniqueKey}
     } catch (err: any) {
         console.log(`Error while uploading object to bucket ${bucketName}`)
